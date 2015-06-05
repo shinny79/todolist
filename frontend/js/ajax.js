39382077019482c2bf6a0c531912fbbdb9c25b84
luoqiang <lq9328@126.com>
2015-06-05 23:00:55 +0800

    ajax update

--- frontend/js/ajax.js
+++ frontend/js/ajax.js
@@ -1,76 +1,93 @@
-
-var XMLHttpReq;  
-function createXMLHttpRequest() {  
-    try {  
-        XMLHttpReq = new ActiveXObject("Msxml2.XMLHTTP");//IE高版本创建XMLHTTP  
-    }  
-    catch(E) {  
-        try {  
-            XMLHttpReq = new ActiveXObject("Microsoft.XMLHTTP");//IE低版本创建XMLHTTP  
-        }  
-        catch(E) {  
-            XMLHttpReq = new XMLHttpRequest();//兼容非IE浏览器，直接创建XMLHTTP对象  
-        }  
-    }  
-  
-}  
-/*
-*	@param {} url 请求的路劲 dataType 请求的数据类型（text） type 请求类型（post） async 同步（true） callback 回调函数  data {} 提交的数据
-*
-*/
-function sendAjaxRequest() {		
-    createXMLHttpRequest(); //创建XMLHttpRequest对象  
-	url = arguments[0].url || null;  
-	dataType = arguments[0].dataType || 'text'; 
-	type = arguments[0].type || 'post'; 
-	async = arguments[0].async || true;
-	callback = arguments[0].callback || null;
-	data = arguments[0].data || null;
-	if(!url) return null;
-	if(data){
-		if(type.toLowerCase() === 'get'){
-			url += url.indexOf('?') == -1 ? '?' : '';
-			if(typeof data != 'object'){
-				try{
-					data =JSON.parse(data);
-				}catch(e){
-					data = eval("("+data+")");
-				}
-			}
-			for(var o in data){
-				url += '&' + o + "=" + data[o];
-			}
-			sendData = null;
-			XMLHttpReq.open(type, url, async); 
-
-		}else{
-    		XMLHttpReq.open(type, url, async); 
-			XMLHttpReq.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-			if(typeof data != 'object'){
-				try{
-					data =JSON.parse(data);
-				}catch(e){
-					data = eval("("+data+")");
-				}
-			}
-			var sendData = '';
-			for(var o in data){
-				sendData += o + "=" + data[o] + '&';  
-			}
-			sendData = sendData.substring(0, sendData.length - 1);
-		}
-	}else{
-		sendData = null;
-	} 
-	XMLHttpReq.send(sendData);         
-    XMLHttpReq.onreadystatechange = function(){				//指定响应函数 
-    	if (XMLHttpReq.readyState == 4) {  
-	        if (XMLHttpReq.status == 200) {  
-	            var res = XMLHttpReq.responseText;
-	 			if(callback){
-	 				callback(res);
-	 			}
+/* 
+* ajax实现
+* @author lq9328@126.com
+* @date 2015/6/5
+* @param url 必选，请求路劲
+* @param type 可选，请求类型，默认post
+* @param data 可选，发送给服务器的数据，json类型
+* @param async 可选，同步，默认同步执行（true）
+* @param callback 可选，function回调函数
+*/ 
+var ajax = function(){
+	var XMLHttpReq = function () {  
+		var xhr = null;
+	    try {  
+	        xhr = new ActiveXObject("Msxml2.XMLHTTP");//IE高版本创建XMLHTTP  
+	    }  
+	    catch(E) {  
+	        try {  
+	            xhr = new ActiveXObject("Microsoft.XMLHTTP");//IE低版本创建XMLHTTP  
 	        }  
-    	} 
-    };
-}  
+	        catch(E) {  
+	            xhr = new XMLHttpRequest();//兼容非IE浏览器，直接创建XMLHTTP对象  
+	        }  
+	    }  
+	  return xhr;
+	}();
+	var parseData = function(data){
+		if(!data) return {};	
+		if(typeof data != 'object'){
+			try{
+				data =JSON.parse(data);
+			}catch(e){
+				data = eval("("+data+")");
+			}
+		}
+		return data;
+	};
+	var isEmptyObject = function( obj ) {
+		var name;
+		for ( name in obj ) {
+			return false;
+		}
+		return true;
+	};
+	return the = {
+	 	request : function(r){
+			var url = arguments[0].url || null;  
+			var dataType = arguments[0].dataType || 'text'; 
+			var type = arguments[0].type || 'post'; 
+			var async = arguments[0].async || true;
+			var callback = arguments[0].callback || null;
+			var data = arguments[0].data || null;
+
+			if(!url) return null;
+			data = parseData(data);
+			if(type.toLowerCase() === 'get'){
+				if(!isEmptyObject(data)){					
+					url += url.indexOf('?') == -1 ? '?' : '&';
+					for(var o in data){
+						url += o + "=" + data[o] + '&';
+					}
+					url = url.substring(0, url.length-1);
+				}
+				var sendData = null;
+				XMLHttpReq.open(type, url, async); 
+			}else{
+	    		XMLHttpReq.open(type, url, async); 
+				XMLHttpReq.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+				if(!isEmptyObject(data)){
+					var sendData = '';
+					for(var o in data){
+						sendData += o + "=" + data[o] + '&';  
+					}
+					sendData = sendData.substring(0, sendData.length - 1);
+				}else{
+					var sendData = null;
+				}
+			}
+			XMLHttpReq.send(sendData);         
+		    XMLHttpReq.onreadystatechange = function(){				//指定响应函数 
+		    	if (XMLHttpReq.readyState == 4) {  
+			        if (XMLHttpReq.status == 200) {  
+			            var res = XMLHttpReq.responseText;
+			 			if(callback){
+			 				callback(res);
+			 			}
+			        }  
+		    	} 
+		    };
+		}
+	}
+
+}();
