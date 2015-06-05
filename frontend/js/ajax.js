da10674d774d5cf955cece7436e57ae646362cd7
luoqiang <lq9328@126.com>
2015-06-05 18:43:46 +0800

    ajax bug修复

--- frontend/js/ajax.js
+++ frontend/js/ajax.js
@@ -14,13 +14,17 @@ function createXMLHttpRequest() {
     }  
   
 }  
+/*
+*	@param {} url 请求的路劲 dataType 请求的数据类型（text） type 请求类型（post） async 同步（true） callback 回调函数  data {} 提交的数据
+*
+*/
 function sendAjaxRequest() {		
     createXMLHttpRequest(); //创建XMLHttpRequest对象  
 	url = arguments[0].url || null;  
 	dataType = arguments[0].dataType || 'text'; 
 	type = arguments[0].type || 'post'; 
 	async = arguments[0].async || true;
-	callBack = arguments[0].callBack || null;
+	callback = arguments[0].callback || null;
 	data = arguments[0].data || null;
 	if(!url) return null;
 	if(data){
@@ -41,19 +45,30 @@ function sendAjaxRequest() {
 
 		}else{
     		XMLHttpReq.open(type, url, async); 
-			XMLHttpReq.setRequestHeader("Content-type","application/raw"); 
-			var sendData = data;
+			XMLHttpReq.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+			if(typeof data != 'object'){
+				try{
+					data =JSON.parse(data);
+				}catch(e){
+					data = eval("("+data+")");
+				}
+			}
+			var sendData = '';
+			for(var o in data){
+				sendData += o + "=" + data[o] + '&';  
+			}
+			sendData = sendData.substring(0, sendData.length - 1);
 		}
 	}else{
 		sendData = null;
-	}       
+	} 
 	XMLHttpReq.send(sendData);         
     XMLHttpReq.onreadystatechange = function(){				//指定响应函数 
     	if (XMLHttpReq.readyState == 4) {  
 	        if (XMLHttpReq.status == 200) {  
 	            var res = XMLHttpReq.responseText;
-	 			if(callBack){
-	 				callBack(res);
+	 			if(callback){
+	 				callback(res);
 	 			}
 	        }  
     	} 
